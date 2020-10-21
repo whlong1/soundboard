df7d5fbf3cd059bc71d8203809a84cf8082f1ead
whlong1 <whunterlong@gmail.com>
2020-10-21 16:53:45 -0400

    fixed display issues on charts.css

--- charts.js
+++ charts.js
@@ -1,11 +1,6 @@
-//music charts
+//itunes album charts
 
 console.log('hello')
-
-// console.log(response)
-// console.log(response.data.trending[0].strArtist)
-// console.log(response.data.trending[0].strAlbum)
-// console.log(response.data.trending[0].strAlbumThumb)
 
 
 const ITUNES_ALBUM_CHARTS= 'https://theaudiodb.com/api/v1/json/523532/trending.php?country=us&type=itunes&format=albums'
@@ -17,10 +12,15 @@ const getChart = async () => {
         const response = await axios.get(ITUNES_ALBUM_CHARTS)
         let trending = response.data.trending
         for (let i = 0; i< trending.length; i++) {
+
             let boxGen = document.createElement('div')
-            let albumTitle = document.createElement('h2')
+            boxGen.classList.add("frame");
+
+            let albumTitle = document.createElement('h3')
             let artistName = document.createElement('p')
             let albumArt = document.createElement('img')
+            albumArt.classList.add("albumBox");
+
             albumTitle.innerText = trending[i].strAlbum
             artistName.innerText = trending[i].strArtist
             albumArt.src = trending[i].strAlbumThumb
@@ -38,24 +38,6 @@ const getChart = async () => {
     }
 }
 
-
-// const makeChart = (data) => {
-//     const chartBox = document.querySelector('.chartBox')
-//     data.forEach((object) => {
-//         console.log(object)
-//         // let boxGen = document.createElement('box')
-//         // boxGen.innerText = `${stat.strArtist}`
-//         // boxGen.setAttribute('artist', stat.artist)
-//         // boxThing.appendChild(boxGen)
-        
-//     })
-//     // chartBox.appendChild(boxThing)
-// }
-
-// const displayChart = () => {
-//     let chartResult
-
-// }
 
 
 window.onload = getChart
