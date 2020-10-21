9f4fe00a759d5184cffd7df6fef9426ee586a87f
whlong1 <whunterlong@gmail.com>
2020-10-21 12:51:57 -0400

    charts page successfully displaying results for top itunes albums

--- charts.js
+++ charts.js
@@ -10,31 +10,52 @@ console.log('hello')
 
 const ITUNES_ALBUM_CHARTS= 'https://theaudiodb.com/api/v1/json/523532/trending.php?country=us&type=itunes&format=albums'
 
+let chartBox = document.querySelector('.chartBox')
 
 const getChart = async () => {
     try {
         const response = await axios.get(ITUNES_ALBUM_CHARTS)
-        makeChart(response.data)
-        console.log(response.data)
+        let trending = response.data.trending
+        for (let i = 0; i< trending.length; i++) {
+            let boxGen = document.createElement('div')
+            let albumTitle = document.createElement('h2')
+            let artistName = document.createElement('p')
+            let albumArt = document.createElement('img')
+            albumTitle.innerText = trending[i].strAlbum
+            artistName.innerText = trending[i].strArtist
+            albumArt.src = trending[i].strAlbumThumb
+            
+            boxGen.appendChild(albumTitle)
+            boxGen.appendChild(albumArt)
+            boxGen.appendChild(artistName)
+            chartBox.appendChild(boxGen)
+            
+
+        }
+        console.log(response.data.trending)
     } catch (error) {
         console.log(error)
     }
 }
 
 
-const makeChart = (data) => {
-    const chartBox = document.querySelector('.chartBox')
-    const boxThing = document.createElement('select')
-    data.forEach((object) => {
-        console.log(object)
-        // let boxGen = document.createElement('box')
-        // boxGen.innerText = `${stat.strArtist}`
-        // boxGen.setAttribute('artist', stat.artist)
-        // boxThing.appendChild(boxGen)
+// const makeChart = (data) => {
+//     const chartBox = document.querySelector('.chartBox')
+//     data.forEach((object) => {
+//         console.log(object)
+//         // let boxGen = document.createElement('box')
+//         // boxGen.innerText = `${stat.strArtist}`
+//         // boxGen.setAttribute('artist', stat.artist)
+//         // boxThing.appendChild(boxGen)
         
-    })
-    // chartBox.appendChild(boxThing)
-}
+//     })
+//     // chartBox.appendChild(boxThing)
+// }
+
+// const displayChart = () => {
+//     let chartResult
+
+// }
 
 
 window.onload = getChart
